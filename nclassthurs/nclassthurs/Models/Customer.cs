10dbd31336b0888de5f392c30d1d031cc2deb044
ltigue <moneynloans@yahoo.com>
2014-07-17 22:06:59 -0400

    Order model created
    replaces all info needed to create the model for order.

---
+++ nclassthurs/nclassthurs/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace nclassthurs.Models
+{
+    public class Customer
+    {
+    }
+}
---
+++ nclassthurs/nclassthurs/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace nclassthurs.Models
+{
+    public class Order
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string OrderID { get; set; }
+        public string Date { get; set; }
+        public int DateTime { get; set; }
+        public decimal Price { get; set; }
+        public string Items { get; set; }
+
+        public virtual ICollection<Customer> Customers { get; set; }
+        public virtual ICollection<Part> Parts { get; set; }
+        
+    }
+}
